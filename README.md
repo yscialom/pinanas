57557253e45017075802d774818d486222293f3c
yscialom <yankel@scialom.org>
2022-01-29 00:58:58 +0100

    Rewrite configuration and installation doc

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 PiNanas
 ========
 
-
 Description
 ===========
 PiNanas is a [NAS](https://en.wikipedia.org/wiki/Network-attached_storage "Network-attached storage"),
-[media center](https://en.wikipedia.org/wiki/Home_theater_PC) and home-hosted services
+[media center](https://en.wikipedia.org/wiki/Home_theater_PC) and home-hosted service set
 designed to run on a small computer like a Raspberry PI. Its feature include:
 
 - Share files in your private network
@@ -18,8 +17,98 @@ designed to run on a small computer like a Raspberry PI. Its feature include:
 
 Installation
 ============
-See [docker-compose](docker-compose/README.md)
 
-Usage
+Requirements
+------------
+
+### Hardware
+
+Pinanas will need a linux-based host to run with:
+- 10GB free disk space
+- 4GB RAM, 8GB suggested
+- An access to Internet
+- Optionnally: a GPU suited to your needs (video transcoding & playing)
+
+### Software
+
+During installation or operation, Pinanas requires:
+- GNU utils
+- python3 and pip
+- docker and docker-compose
+- a wildcard (sub)domain name (e.g. `*.home.example.com`)
+
+
+Download
+--------
+
+### via git
+From your Pinanas host, anywhere:
+```bash
+git clone --depth 1 --branch master https://github.com/yscialom/pinanas.git
+```
+
+### direct download
+Go and donwload our [latest release](https://github.com/yscialom/pinanas/releases) Source code.
+Unzip it anywhere.
+
+
+Settings
+--------
+
+Create the installation directory for Pinanas, e.g. in `/opt/pinanas` and go there:
+```bash
+sudo mkdir -p /opt/pinanas
+cd /opt/pinanas
+```
+
+### Define your settings
+Create a file `settings.yml` from `src/settings.yml.sample` and fill in all values:
+```bash
+cp /path/to/pinanas/src/settings.yml.sample settings.yml
+nano settings.yml
+```
+See [Get your DNS API keys](#get-your-dns-api-keys) below for how to set `PINANAS_DNS_PROVIDER_VARS`.
+
+### Get your DNS API keys
+
+#### OVH
+
+Go to [OVH's API Key creation page](https://eu.api.ovh.com/createToken/) and fill in:
+- "Account ID or email address": your OVH account name
+- "Password": your OVH account password
+- "Script Name": a title for your usage, e.g. "pinanas"
+- "Script Description": a description for your usage, e.g. "Handle dns challenge for wildcard certificate issue for **.domainname"
+- "Validity": "Unlimited"
+- "Rights":
+-- "GET /domain/zone/*"
+-- "PUT /domain/zone/*"
+-- "POST /domain/zone/*"
+-- "DELETE /domain/zone/*"
+And click "Create keys".
+
+You will be given an Application Key, an Application Secret and a Consumer Key. Keep those secret and safe as you cannot retrieve them.
+
+Update `settings.yml` and add the following values:
+```yaml
+PINANAS_DNS_PROVIDER_VARS:
+  - { name: OVH_APPLICATION_KEY, value: <Your Application Key> }
+  - { name: OVH_APPLICATION_SECRET, value: <Your Application Secret> }
+  - { name: OVH_CONSUMER_KEY, value: <Your Consumer Key> }
+  - { name: OVH_ENDPOINT, value: <Your OVH zone, e.g. ovh-eu> }
+```
+
+Install
+-------
+
+From your installation directory, run:
+```bash
+/path/to/pinanas/src/configure.sh
+```
+If you made important changes and want to regenerate pinanas, run with `--force`.
+
+Your installation is now complete.
+
+Start
 =====
-TODO
+
+From your installation directory, run `docker-compose up -d`.
---
+++ src/configure.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+ROOT="$(dirname "$(readlink -f "${0}")")"
+DIST="$(readlink -f ${PWD})"
+VENV="${DIST}/.venv"
+
+
+#
+## log
+#
+
+function color()  { echo -e "\033[${1}m${2}\033[0m" ; }
+function red()    { color "0;31" "$*" ; }
+function yellow() { color "0;33" "$*" ; }
+function green()  { color "0;32" "$*" ; }
+
+function log ()  { echo "$(basename "$0") [$1] $2" >&2 ; }
+function info()  { log " $(green INFO)"  "$*" ; }
+function cont()  { log "     "           "$*" ; }
+function warn()  { log " $(yellow WARN)" "$*" ; }
+function error() { log "$(red ERROR)"    "$*" ; }
+function fatal() { log "$(red FATAL)"    "$(red $*)" ; }
+
+
+#
+## Check context
+#
+
+if [[ ! -f "${DIST}/settings.yml" ]] ; then
+    fatal "${DIST}/settings.yml no such file."
+    cont "Copy ${ROOT}/settings.yml.sample to ${DIST}/settings.yml and fill in your personnal configuration."
+    exit 1
+fi
+
+if ! command -v pip >/dev/null ; then
+    fatal "command pip not found."
+    cont "This script requires pip. See <https://pypi.org/project/pip/> for help installing it."
+    exit 1
+fi
+
+if [[ ${1} == "--force" ]] ; then
+    OPT_FORCE=true
+    rm -rf -- "${VENV}"
+fi
+
+
+#
+## Install tools
+#
+
+if ! python3 -m venv "${VENV}" ; then
+    error "python -m venv not found."
+    cont "This script requires python virtual environments. See <https://docs.python.org/3/library/venv.html> for help installing it."
+    exit 1
+fi
+
+source "${VENV}"/bin/activate
+
+python3 -m pip install --upgrade pip
+pip install ansible==2.9
+
+
+#
+## Apply private configuration
+#
+
+# Create playbook file
+playbook="${VENV}"/ansible/playbook.yml
+mkdir -p $(dirname "${playbook}")
+
+# Install plugins
+plugin_files="\
+:${ROOT}/utils/ansible_passwords.py\
+"
+plugins_dir="$(dirname ${playbook})/filter_plugins"
+mkdir -p "${plugins_dir}"
+
+echo "${plugin_files}" | tr : '\n' | while read file ; do
+    if [[ -r "${file}" ]] ; then
+        pip install -r "$(dirname "${file}")"/requirements.txt
+        cp "${file}" "${plugins_dir}/$(basename ${file})"
+    fi
+done
+
+# Playbook header
+cat > ${playbook} <<EOH
+---
+- hosts: localhost
+  gather_facts: yes
+  tasks:
+  - include_vars: ${DIST}/settings.yml
+  - name: ensure application directories exist
+    file:
+      path: "${DIST}/{{ item[0] }}/{{ item[1] }}"
+      state: directory
+      mode: 0755
+    loop: "{{ ['dhcpd', 'traefik', 'authelia', 'adguardhome'] | product(['config', 'data']) | list }}"
+  - name: ensure traefik/acme.json exists
+    file:
+      path: "${DIST}/traefik/data/acme.json"
+      state: touch
+      mode: 0600
+EOH
+
+# Playbook tasks
+for j2 in $(find "${ROOT}/templates" -type f -name "*.j2") ; do
+    destdir="$(dirname "${DIST}/${j2#${ROOT}/templates/}")"
+    filename="$(basename "${j2%.j2}")"
+    cat >> ${playbook} <<EOT
+  # handle ${j2}
+  - name: ensure ${destdir} exists
+    file:
+      path: "${destdir}"
+      state: directory
+  - name: create ${destdir}/${filename} from settings
+    template:
+      src:  "${j2}"
+      dest: "${destdir}/${filename}"
+      force: $([[ ${OPT_FORCE} == true ]] && echo yes || echo no)
+    delegate_to: localhost
+
+EOT
+done
+
+
+#
+## Install
+#
+
+EXTRA_VARS="{
+  PUID: $(id -u),
+  PGID: $(id -g)
+}"
+
+export ANSIBLE_LOCALHOST_WARNING=false
+ansible-playbook \
+    --inventory /dev/null \
+    --extra-vars "${EXTRA_VARS}" \
+    ${playbook}
+
+ansible_status=$?
+if [[ ${ansible_status} != 0 ]] ; then
+    error "ansible returned error code ${ansible_status}."
+    exit ${ansible_status}
+fi
+
+info "Configuration successful."
+cont "You can clean work files with ./distclean.sh"
+cont "Start your services with docker-compose up -d"
+
+if [[ -n $(ss -Hul "sport = :domain") ]] ; then
+    warn "Port udp/53 appears to be already in use. You should free it before starting pinanas."
+    cont "On Ubuntu 18.04+ systems, in order to free udp/53 you can disable your local DNS cache server:"
+    cont "    sudo mkdir -p /etc/systemd/resolved.conf.d"
+    cont "    cat | sudo tee -a /etc/systemd/resolved.conf.d/disable-for-pinanas.conf"
+    cont "    [Resolve]"
+    cont "    DNSStubListener=no"
+    cont "    ^D"
+    cont "    sudo systemctl force-reload systemd-resolved"
+fi
+
+
+#
+## Clean
+#
+
+cat > "${DIST}/distclean.sh" <<EOF
+#!/bin/bash
+rm -rf -- "${VENV}"
+rm -- \$0
+EOF
+chmod +x "${DIST}/distclean.sh"
