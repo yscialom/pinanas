24f025fe522b19878249a3a9f1f40595ac50f699
yscialom <yankel@scialom.org>
2022-02-13 12:39:14 +0100

    fixup! Test: traefik dashboard http

--- test/traefik-dashboard.sh
+++ test/traefik-dashboard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source web-functions.sh
+TEST_DIR="$(dirname "$($readlink -f "$0")")"
+source "${TEST_DIR}/web-functions.sh"
 
 domain="pinanas-ci.scialom.org"
 web_expect "http://traefik-dashboard.${domain}" -c 302 -H "Location: https://traefik-dashboard.${domain}/"
