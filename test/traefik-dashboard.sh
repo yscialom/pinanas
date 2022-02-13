2571da9a5d0cceda9326d5d58278eb8d2e5e0afc
yscialom <yankel@scialom.org>
2022-02-13 14:28:55 +0100

    trigger

--- test/traefik-dashboard.sh
+++ test/traefik-dashboard.sh
@@ -4,4 +4,6 @@ TEST_DIR="$(dirname "$(readlink -f "$0")")"
 source "${TEST_DIR}/web-functions.sh"
 
 domain="pinanas-ci.scialom.org"
+
+docker run --rm curlimages/curl:7.77.0 -v "http://traefik-dashboard.${domain}"
 web_expect "http://traefik-dashboard.${domain}" -c 302 -H "Location: https://traefik-dashboard.${domain}/"
