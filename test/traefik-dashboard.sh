007d8f78f3780afc4678cac8bb4f158295b9ad41
yscialom <yankel@scialom.org>
2022-02-15 23:35:16 +0100

    Add more traefik tests

--- test/traefik-dashboard.sh
+++ test/traefik-dashboard.sh
@@ -4,5 +4,12 @@ TEST_DIR="$(dirname "$(readlink -f "$0")")"
 source "${TEST_DIR}/web-functions.sh"
 
 domain="pinanas-ci.scialom.org"
+traefik="traefik-dashboard.${domain}"
 
-web_expect "http://traefik-dashboard.${domain}" -c 302 -H "Location: https://traefik-dashboard.${domain}/"
+# http -> https
+web_expect "http://${traefik}"  -c 302 -r "https://${traefik}/"
+web_expect "httpd://${traefik}" -c 302 -r "https://${traefik}/dashboard/"
+web_expect "httpd://${traefik}/dashboard/" -c 200
+
+# api
+api_expect "httpd://${traefik}/" -l 13
--- test/web-functions.sh
+++ test/web-functions.sh
@@ -2,24 +2,15 @@ function error () {
     echo "$(basename $0): error: $1" >&2
 }
 
-function test_exit_status () {
-    local name=$1
-    local actual=$2
-    local expected=$3
-    if [[ ${actual} != ${expected} ]] ; then
-        error "'${name}': exit_status: ${actual}; expected: ${expected}."
-        exit 2
-    fi
-}
+function test_field () {
+    local field="${1}"
+    local url="${2}"
+    local actual="${3}"
+    local expected="${4}"
 
-function test_http_code () {
-    local url="${1}"
-    local actual=$2
-    local expected=$3
-
-    if [[ ${actual} != ${expected} ]] ; then
-        error "'${url}': http_code: ${actual}; expected: ${expected}."
-        exit 3
+    if [[ "${actual}" != "${expected}" ]] ; then
+        error "'${url}': ${field}: '${actual}'; expected: '${expected}'."
+        return 10
     fi
 }
 
@@ -28,11 +19,11 @@ function web_expect () {
     shift
 
     # get tests options
-    while getopts "c:H:" opt ; do
+    while getopts "c:r:" opt ; do
         case "${opt}" in
         c) local expected_http_code=${OPTARG} ;;
-        H) local expected_header=${OPTARG} ;;
-        *) echo "$0: error: Unknown option ${opt}." ; exit 1 ;;
+        r) local expected_redirect_url=${OPTARG} ;;
+        *) error "Unknown option ${opt}." ; exit 1 ;;
         esac
     done
 
@@ -41,14 +32,47 @@ function web_expect () {
     docker run --rm --net host curlimages/curl:7.77.0 --silent --output /dev/null --write-out '%{json}' "${url}" >${response}
 
     # check curl status
-    test_exit_status curl $? 0 || exit $?
+    test_field exit_status $? 0 || exit $?
 
     # check response
     if [[ -n ${expected_http_code} ]] ; then
         local actual_http_code=$(jq .http_code <${response})
-        test_http_code "${url}" ${actual_http_code} ${expected_http_code} || exit $?
+        test_field http_code "${url}" ${actual_http_code} ${expected_http_code} || exit $?
     fi
 
     # check redirect
-    # TODO
+    if [[ -n ${expected_redirect_url} ]] ; then
+        local actual_redirect_url=$(jq .redirect_url <${response})
+        test_field redirect_url "${url}" ${actual_redirect_url} ${expected_redirect_url} || exit $?
+    fi
+
+    rm -- "${response}"
+}
+
+function api_expect () {
+    local url="${1}"
+    shift
+
+    # get tests options
+    while getopts "l:" opt ; do
+        case "${opt}" in
+        l) local expected_length=${OPTARG} ;;
+        *) error "Unknown option ${opt}." ; exit 1 ;;
+        esac
+    done
+
+    # request
+    response=$(mktemp)
+    docker run --rm --net host curlimages/curl:7.77.0 --silent "${url}" >${response}
+
+    # check curl status
+    test_field exit_status $? 0 || exit $?
+
+    # check length
+    if [[ -n ${expected_length} ]] ; then
+        local actual_length=$(jq '. | length' <${response})
+        test_field length "${url}" ${actual_length} ${expected_length} || exit $?
+    fi
+
+    rm -- "${response}"
 }
