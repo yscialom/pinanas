acb16941f4c83ae1e04cf4017f1b43a4a82554ea
yscialom <yankel@scialom.org>
2022-03-06 11:09:21 +0100

    trigger

--- test/web-functions.sh
+++ test/web-functions.sh
@@ -1,6 +1,14 @@
-function error () {
-    echo "$(basename $0): error: $1" >&2
-}
+color()  { printf "\033[%sm${2}\033[0m\n" "${1}"; }
+red()    { color "0;31" "$*" ; }
+yellow() { color "0;33" "$*" ; }
+green()  { color "0;32" "$*" ; }
+
+log ()  { echo "$(basename "$0") [$1] $2" >&2 ; }
+info()  { log " $(green INFO)"  "$*" ; }
+cont()  { log "     "           "$*" ; }
+warn()  { log " $(yellow WARN)" "$*" ; }
+error() { log "$(red ERROR)"    "$*" ; }
+fatal() { log "$(red FATAL)"    "$(red "$@")" ; }
 
 function curl_ () {
     docker run --rm --net host \
