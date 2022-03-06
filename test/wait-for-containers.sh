785df22cf0942b932f6072de1a9a52d8d73f52e5
yscialom <yankel@scialom.org>
2022-03-06 17:46:33 +0100

    Wait for containers

---
+++ test/wait-for-containers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+TEST_DIR="$(dirname "$(readlink -f "$0")")"
+source "${TEST_DIR}/web-functions.sh"
+
+function wait_for_container {
+    local container_name="${1}"
+    local container_id="$(docker inspect "${container_name}" --format '{{ .Id }}')"
+    info "Waiting for container ${container_name} (id: ${container_id})"
+    local waiting_done="false"
+    while [[ ${waiting_done} != "true" ]] ; do
+        local container_state="$(docker inspect "${container_id}" --format '{{ .State.Status }}')"
+        if [[ "${container_state}" == "running" ]]; then
+            local health_status="$(docker inspect "${container_id}" --format '{{ .State.Health.Status }}')"
+            cont "${container_name}: container_state=${container_state}, health_status=${health_status}"
+            if [[ ${health_status} == "healthy" ]]; then
+                waiting_done="true"
+            fi
+        else
+            cont "${container_name}: container_state=${container_state}"
+            waiting_done="true"
+        fi
+        sleep 1;
+    done;
+}
+
+wait_for_container adguardhome
+wait_for_container authelia
+wait_for_container dhcpd
+wait_for_container heimdall
+wait_for_container netdata
+wait_for_container nextcloud
+wait_for_container traefik
