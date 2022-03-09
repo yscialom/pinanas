60b635753e0ad33a9fa60b39441191237b29511f
yscialom <yankel@scialom.org>
2022-03-09 22:45:07 +0100

    trigger

--- test/traefik-dashboard.sh
+++ test/traefik-dashboard.sh
@@ -12,6 +12,9 @@ web_expect "http://${traefik}"  -c 302 -r "https://${traefik}/"
 web_expect "https://${traefik}" -c 302 -r "https://${traefik}/dashboard/"
 web_expect "https://${traefik}/dashboard/" -c 200
 
+#tmp
+curl -s "https://${traefik}/api/http/services
+
 # api
 api_expect "https://${traefik}/api/overview" -q '.http.services.total==10'
 api_expect "https://${traefik}/api/overview" -q '.http.middlewares.total==12'
@@ -19,5 +22,5 @@ api_expect "https://${traefik}/api/overview" -q '.http.routers.total==12'
 api_expect "https://${traefik}/api/overview" -q '.udp.routers.total==1'
 api_expect "https://${traefik}/api/overview" -q '.udp.services.total==1'
 for entity in .{http,udp,tcp}.{services,middlewares,routers} do
-    api_expect "https://${traefik}/api/overview" -q '${entity}.warnings==0 and ${entity}.errors==0'
+    api_expect "https://${traefik}/api/overview" -q "${entity}.warnings==0 and ${entity}.errors==0"
 done
