87a6e5c3049e921afe087feb6e4293215be0a5f1
yscialom <yankel@scialom.org>
2022-06-20 14:15:56 +0200

    test

--- test/nextcloud-install.sh
+++ test/nextcloud-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIST_DIR="$(readlink -f "${1}")"
 
 TEST_DIR="$(dirname "$(readlink -f "$0")")"
 source "${TEST_DIR}/web-functions.sh"
@@ -17,7 +18,7 @@ function test_install () {
         occ ${command} | sed -n 's/ *- '"${fieldname}"': *\([0-9a-zA-Z._-]\+\)\r\?$/\1/p'
     }
 
-    local expected_nextcloud_version=$(sed -n 's/^FROM .*nextcloud:\([0-9.]\+\)$/\1/p' nextcloud/build/Dockerfile)
+    local expected_nextcloud_version=$(sed -n 's/^FROM .*nextcloud:\([0-9.]\+\)$/\1/p' "${DIST_DIR}/nextcloud/build/Dockerfile")
     test_field "installed"   "occ status" "$(command_field installed     status)" "true"
     test_field "version"     "occ status" "$(command_field versionstring status)" "${expected_nextcloud_version}"
     test_field "maintenance" "occ status" "$(command_field maintenance   status)" "false"
@@ -27,7 +28,8 @@ function test_install () {
 function test_oidc () {
     local expected_discovery_endpoint="https://${authelia}/.well-known/openid-configuration"
     local actual_discovery_endpoint=$(occ user_oidc:provider --output=json --ansi \
-        | jq -r '.[] | select(.identifier=="Authelia") .discoveryEndpoint')
+        | jq -r '.[] | select(.identifier=="Authelia") .discoveryEndpoint' \
+        || true )
         test_field "openid connect" "${actual_discovery_endpoint}" "${expected_discovery_endpoint}"
 }
 
