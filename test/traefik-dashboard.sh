45418364ac16caa0055528ca1bebf0ea7f23a898
Yankel Scialom <yankel@scialom.org>
2022-11-04 21:53:20 +0100

    Fix version bump errors for traefik, nextcloud, and fix network settings detection (#55)
    * Fix migration error from traefik v2.4 to v2.9
    * Fix default network settings detection
    * Fix unrelated nextcloud issues

--- test/traefik-dashboard.sh
+++ test/traefik-dashboard.sh
@@ -8,7 +8,7 @@ domain="pinanas-ci.scialom.org"
 traefik="traefik-dashboard.${domain}"
 
 # http -> https
-web_expect "http://${traefik}"  -c 302 -r "https://${traefik}/"
+web_expect "http://${traefik}"  -c 301 -r "https://${traefik}/"
 web_expect "https://${traefik}" -c 302 -r "https://${traefik}/dashboard/"
 web_expect "https://${traefik}/dashboard/" -c 200
 
