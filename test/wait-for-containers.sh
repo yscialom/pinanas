7b145408408bbe69c7ff34b88533d4a1ebbd7b91
yscialom <yankel@scialom.org>
2022-11-19 19:45:13 +0100

    trigger

--- test/wait-for-containers.sh
+++ test/wait-for-containers.sh
@@ -31,5 +31,8 @@ function wait_for_container {
 }
 
 for service in $(docker-compose ps --services) ; do
-    wait_for_container "${service}"
+    if [[ -x "${TEST_DIR}/wait-for-${service}.sh" ]]
+    then "${TEST_DIR}/wait-for-${service}.sh"
+    else wait_for_container "${service}"
+    fi
 done
---
+++ test/wait-for-nextcloud.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TEST_DIR="$(dirname "$(readlink -f "$0")")"
+source "${TEST_DIR}/web-functions.sh"
+
+function wait-for-nextcloud () {
+    local waiting_done="false"
+    while [[ ${waiting_done} != "true" ]] ; do
+        [[ -f nextcloud/data/generated-config/installed ]] && waiting_done="true"
+    done
+}
+
+wait-for-nextcloud
