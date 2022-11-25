525de4b679fe09a7bcc4fa2bb1ce6c627aa241ca
glevil <63508453+glevil@users.noreply.github.com>
2022-11-25 22:52:41 +0100

    Feature/fail2ban (#62)
    * Add fail2ban conf *.local files and modify docker-compose
    
    * Output Authelia logs to be read by Fail2ban
    
    * fix jails activation for sshd - authelia - nextcloud
    
    * fix
    
    * fix - fail2ban working to protect app through docker
    
    * fix - error on lauch for fail2ban if access.log (traefik) not here
    
    * Update docker-compose.yml.j2
    
    * Fail2ban - version : 0.11.2
    
    * Update continuous-integration.yml
    
    * Fix unit test
    
    Co-authored-by: yscialom <yankel@scialom.org>

--- test/wait-for-containers.sh
+++ test/wait-for-containers.sh
@@ -32,5 +32,7 @@ function wait_for_container {
 
 for service in $(docker-compose ps --services) ; do
     wait_for_container "${service}"
-    [[ -x "${TEST_DIR}/wait-for-${service}.sh" ]] && "${TEST_DIR}/wait-for-${service}.sh"
+    if [[ -x "${TEST_DIR}/wait-for-${service}.sh" ]]
+    then "${TEST_DIR}/wait-for-${service}.sh"
+    fi
 done
