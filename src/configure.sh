b11b89eccc75ff81b65ace07284320f2b4dca57b
glevil <levillain.gaetan@hotmail.fr>
2022-12-01 22:48:11 +0100

    Option 2 - need to be helped

--- src/configure.sh
+++ src/configure.sh
@@ -34,7 +34,7 @@ configure () {
 report () {
     info "Configuration successful."
     cont "You can clean work files with ./distclean.sh"
-    cont "Start your services with docker-compose --profile OK up -d"
+    cont "Start your services with docker-compose up -d"
 
     command -v netstat >/dev/null || return
     if netstat -lan | grep -qE ':53\W' ; then
@@ -49,4 +49,7 @@ report () {
 }
 
 configure "$@"|| exit $?
+
+. ${PINANAS_SRC}/build/configure-dc.sh
+
 report
