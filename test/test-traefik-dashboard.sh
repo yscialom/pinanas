7006499e8b4faacb1baa4020af36a4bceb956a53
glevil <levillain.gaetan@hotmail.fr>
2023-01-14 22:15:40 +0100

    fix custom ports for ext services

--- test/test-traefik-dashboard.sh
+++ test/test-traefik-dashboard.sh
@@ -6,8 +6,10 @@ DIST_DIR="$(readlink -f "${1}")"
 source "${TEST_DIR}/web-functions.sh"
 
 domain="pinanas-ci.scialom.org"
-traefik_unsecure="traefik-dashboard.${domain}:8080"
-traefik_secure="traefik-dashboard.${domain}:8443"
+http_port=":8080"
+https_port=":8443"
+traefik_http="traefik-dashboard.${domain}${http_port}"
+traefik_https="traefik-dashboard.${domain}${https_port}"
 
 # setup external services
 trap "docker stop pinanas-ci-ext-services && docker rmi httpd:2-alpine" EXIT
@@ -23,24 +25,24 @@ docker run \
 sleep 10
 
 # http -> https
-web_expect "http://${traefik_unsecure}" -c 301 -r "https://${traefik_secure}/"
-web_expect "https://${traefik_secure}" -c 302 -r "https://${traefik_secure}/dashboard/"
-web_expect "https://${traefik_secure}/dashboard/" -c 200
+web_expect "http://${traefik_http}" -c 301 -r "https://${traefik_https}/"
+web_expect "https://${traefik_https}" -c 302 -r "https://${traefik_https}/dashboard/"
+web_expect "https://${traefik_https}/dashboard/" -c 200
 
 # api
-api_expect "https://${traefik_secure}/api/overview" -q '.http.services.total==17'
-api_expect "https://${traefik_secure}/api/overview" -q '.http.middlewares.total==15'
-api_expect "https://${traefik_secure}/api/overview" -q '.http.routers.total==22'
-api_expect "https://${traefik_secure}/api/overview" -q '.udp.routers.total==1'
-api_expect "https://${traefik_secure}/api/overview" -q '.udp.services.total==1'
+api_expect "https://${traefik_https}/api/overview" -q '.http.services.total==17'
+api_expect "https://${traefik_https}/api/overview" -q '.http.middlewares.total==15'
+api_expect "https://${traefik_https}/api/overview" -q '.http.routers.total==22'
+api_expect "https://${traefik_https}/api/overview" -q '.udp.routers.total==1'
+api_expect "https://${traefik_https}/api/overview" -q '.udp.services.total==1'
 for entity in .{http,udp,tcp}.{services,middlewares,routers} ; do
-    api_expect "https://${traefik_secure}/api/overview" -q "${entity}.warnings==0 and ${entity}.errors==0"
+    api_expect "https://${traefik_https}/api/overview" -q "${entity}.warnings==0 and ${entity}.errors==0"
 done
 
 # external services
-web_expect "http://ext1.${domain}" -c 301 -r "https://ext1.${domain}/"
-html_expect "https://ext1.${domain}/" -r '<html><body><h1>It works!</h1></body></html>'
+web_expect "http://ext1.${domain}${http_port}" -c 301 -r "https://ext1.${domain}${https_port}/"
+html_expect "https://ext1.${domain}${https_port}/" -r '<html><body><h1>It works!</h1></body></html>'
 for sid in $(seq 2 4) ; do
-    web_expect "http://ext${sid}.${domain}" -c 301 -r "https://ext${sid}.${domain}/"
-    html_expect "https://ext${sid}.${domain}/" -r '<title>Login - Authelia</title>'
+    web_expect "http://ext${sid}.${domain}${http_port}" -c 301 -r "https://ext${sid}.${domain}${https_port}/"
+    html_expect "https://ext${sid}.${domain}${https_port}/" -r '<title>Login - Authelia</title>'
 done
