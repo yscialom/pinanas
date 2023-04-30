8e16137a5cbaab399be3f317aab3b9dbbf19285b
yscialom <yankel@scialom.org>
2023-04-30 20:27:40 +0200

    Add first settings-validator unit test

---
+++ test/test-settings-validator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+TEST_DIR="$(dirname "$(readlink -f "$0")")"
+DIST_DIR="$(readlink -f "${1}")"
+TMP_FILE="$(mktemp)"
+trap "rm -f -- ${TMP_FILE}" EXIT
+
+source "${TEST_DIR}/web-functions.sh"
+source "${DIST_DIR}/.venv/bin/activate"
+
+function validate () {
+    local patch="${1}"
+    local settings_validator="$(readlink -f ${TEST_DIR}/../src/utils/settings-validator)"
+    patch -p1 "${patch}" <"${TEST_DIR}/settings.yaml.d/ci.yaml" >"${TMP_FILE}"
+    "${settings_validator}/validate.py" -s "${settings_validator}/schema.json" -y "${TMP_FILE}"
+}
+
+validate "${TEST_DIR}/settings.yaml.d/pinanas.domain-empty.patch"
