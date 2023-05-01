337135d60b8626db9e052621c70da6282f5b8d1b
yscialom <yankel@scialom.org>
2023-05-01 14:19:35 +0200

    Add timezones + pinanas/users

--- test/test-settings-validator.sh
+++ test/test-settings-validator.sh
@@ -10,6 +10,9 @@ trap "rm -f -- ${SCRIPT_FILE} ${SETTINGS_FILE}" EXIT
 source "${TEST_DIR}/web-functions.sh"
 source "${DIST_DIR}/.venv/bin/activate"
 
+#
+## === prepare() ===
+#
 function prepare () {
     cat >"${SCRIPT_FILE}" <<EOF
 #!/usr/bin/env python3
@@ -32,9 +35,12 @@ for field in fields[:-1]:
 node[last_field] = value
 print(yaml.dump(settings, default_flow_style=False, sort_keys=False))
 EOF
-    chmod +x "${SCRIPT_FILE}"
+    chmod +x -- "${SCRIPT_FILE}"
 }
 
+#
+## === check() ===
+#
 function check () {
     local path="${1}"
     local value="${2}"
@@ -52,6 +58,9 @@ function check () {
     test_field "'${path} = \"${value}\"'" "settings.yaml validation" ${actual} ${expected}
 }
 
+#
+## === ENTRY POINT ===
+#
 prepare
 
 check "pinanas/domain" "example.com"                valid
@@ -78,6 +87,34 @@ check "pinanas/master_secret" "s3cr3t"                                  valid
 check "pinanas/master_secret" "azAZ09 /*-+&~#'([{|\`\\^@])}=$%!:;,?."   valid
 check "pinanas/master_secret" "$(head -c 100 </dev/random | base64)"    valid
 
-# TODO timezone
+check "pinanas/timezone" "Europe/Paris"             valid
+check "pinanas/timezone" "America/Louisville"       valid
+check "pinanas/timezone" "US/Samoa"                 valid
+check "pinanas/timezone" "Kwajalein"                valid
+check "pinanas/timezone" "Europe/Stockholm"         valid
+check "pinanas/timezone" "Asia/Makassar"            valid
+check "pinanas/timezone" "Africa/Ouagadougou"       valid
+check "pinanas/timezone" "America/Porto_Velho"      valid
+check "pinanas/timezone" "America/Merida"           valid
+check "pinanas/timezone" "Europe/Busingen"          valid
+check "pinanas/timezone" "Antarctica/Vostok"        valid
+check "pinanas/timezone" "Europe/Kiev"              valid
+check "pinanas/timezone" "CET"                      valid
+check "pinanas/timezone" "Etc/GMT+12"               valid
+check "pinanas/timezone" "Etc/GMT-14"               valid
+check "pinanas/timezone" "Etc/GMT+0"                valid
+check "pinanas/timezone" "Etc/GMT-0"                valid
+check "pinanas/timezone" "Etc/UTC"                  valid
+check "pinanas/timezone" "Etc/Zulu"                 valid
+check "pinanas/timezone" "Europe/Mont_Saint_Michel" invalid
+check "pinanas/timezone" "Asia/New_York"            invalid
+check "pinanas/timezone" 42                         invalid
 
-check "pinanas/users" '[{ "login": "John Doe (JDO)", "password": "azAZ09 /*-+&~#([{|\\`^@])}=$%!:;,?.", "email": "john-john.doe+label@gmail.com" }]' valid
+check "pinanas/users" '[{ "login": "johndoe", "password": "azAZ09 /*-+&~#([{|\\`^@])}=$%!:;,?.", "email": "john-john.doe+label@gmail.com" }]'   valid
+check "pinanas/users" '[{ "login": "johndoe", "password": "qwerty", "fullname": "John Doe (JDO)", "email": "john-john.doe+label@gmail.com" }, \
+                        { "login": "janedo", "password": "azerty", "email": "jane@do.it" }]'                                                    valid
+check "pinanas/users" '[]'                                                                                                                      valid
+check "pinanas/users" '{ "login": "janedo", "password": "azerty", "email": "jane@do.it" }'                                                      invalid
+check "pinanas/users" '[{ "login": "janedo", "password": "azerty" }]'                                                                           invalid
+check "pinanas/users" '[{ "login": "janedo", "email": "jane@do.it"" }]'                                                                         invalid
+check "pinanas/users" '[{ "password": "azerty", "email": "jane@do.it" }]'                                                                       invalid
